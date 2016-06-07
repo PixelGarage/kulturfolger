e1cb458a43fda4d13a68d671534878451bddf91d
Ralph Moser <ralph@ramosoft.ch>
2016-06-07 17:26:05 +0200

    - modules added - libraries cleanup - ckeditor styles added - db updated

---
+++ all/themes/pixelgarage/ckeditor.styles.js
@@ -0,0 +1,122 @@
+/*
+Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+/*
+ * This file is used/requested by the 'Styles' button.
+ * The 'Styles' button is not enabled by default in DrupalFull and DrupalFiltered toolbars.
+ */
+if(typeof(CKEDITOR) !== 'undefined') {
+    CKEDITOR.addStylesSet( 'drupal',
+    [
+            /* Kulturfolger styles */
+            { name : 'kf-normal',
+                    element : 'p',
+                    attributes : {
+                            'class' : 'kf-normal' }
+            },
+            { name : 'kf-style1',
+                    element : 'p',
+                    attributes : {
+                            'class' : 'kf-style1' }
+            },
+            { name : 'kf-style2',
+                    element : 'p',
+                    attributes : {
+                            'class' : 'kf-style2' }
+            },
+            { name : 'kf-style3',
+                    element : 'p',
+                    attributes : {
+                            'class' : 'kf-style3' }
+            },
+            { name : 'kf-style4',
+                    element : 'p',
+                    attributes : {
+                            'class' : 'kf-style4' }
+            },
+            { name : 'kf-style5',
+                    element : 'p',
+                    attributes : {
+                            'class' : 'kf-style5' }
+            }
+
+            /* Block Styles */
+
+            // These styles are already available in the "Format" drop-down list, so they are
+            // not needed here by default. You may enable them to avoid placing the
+            // "Format" drop-down list in the toolbar, maintaining the same features.
+            /*
+            { name : 'Paragraph'		, element : 'p' },
+            { name : 'Heading 1'		, element : 'h1' },
+            { name : 'Heading 2'		, element : 'h2' },
+            { name : 'Heading 3'		, element : 'h3' },
+            { name : 'Heading 4'		, element : 'h4' },
+            { name : 'Heading 5'		, element : 'h5' },
+            { name : 'Heading 6'		, element : 'h6' },
+            { name : 'Preformatted Text', element : 'pre' },
+            { name : 'Address'			, element : 'address' },
+            */
+
+            /* Inline Styles */
+
+            // These are core styles available as toolbar buttons. You may opt enabling
+            // some of them in the "Styles" drop-down list, removing them from the toolbar.
+            /*
+            { name : 'Strong'			, element : 'strong', overrides : 'b' },
+            { name : 'Emphasis'			, element : 'em'	, overrides : 'i' },
+            { name : 'Underline'		, element : 'u' },
+            { name : 'Strikethrough'	, element : 'strike' },
+            { name : 'Subscript'		, element : 'sub' },
+            { name : 'Superscript'		, element : 'sup' },
+
+            { name : 'Marker: Yellow'	, element : 'span', styles : { 'background-color' : 'Yellow' } },
+            { name : 'Marker: Green'	, element : 'span', styles : { 'background-color' : 'Lime' } },
+
+            { name : 'Big'				, element : 'big' },
+            { name : 'Small'			, element : 'small' },
+            { name : 'Typewriter'		, element : 'tt' },
+
+            { name : 'Computer Code'	, element : 'code' },
+            { name : 'Keyboard Phrase'	, element : 'kbd' },
+            { name : 'Sample Text'		, element : 'samp' },
+            { name : 'Variable'			, element : 'var' },
+
+            { name : 'Deleted Text'		, element : 'del' },
+            { name : 'Inserted Text'	, element : 'ins' },
+
+            { name : 'Cited Work'		, element : 'cite' },
+            { name : 'Inline Quotation'	, element : 'q' },
+
+            { name : 'Language: RTL'	, element : 'span', attributes : { 'dir' : 'rtl' } },
+            { name : 'Language: LTR'	, element : 'span', attributes : { 'dir' : 'ltr' } },
+             */
+
+            /* Object Styles */
+/*
+
+            {
+                    name : 'Image on Left',
+                    element : 'img',
+                    attributes :
+                    {
+                            'style' : 'padding: 5px; margin-right: 5px',
+                            'border' : '2',
+                            'align' : 'left'
+                    }
+            },
+
+            {
+                    name : 'Image on Right',
+                    element : 'img',
+                    attributes :
+                    {
+                            'style' : 'padding: 5px; margin-left: 5px',
+                            'border' : '2',
+                            'align' : 'right'
+                    }
+            }
+            */
+    ]);
+}
